53aada288ad5ab7f89c06a3d29d1df89e0ae3b77
zen <inarrituzanetti@outlook.com>
2021-08-23 23:44:03 -0500

    code finish

--- blockchain_A_Z/Module 2 - Create a Cryptocurrency/quetzalCoin.py
+++ blockchain_A_Z/Module 2 - Create a Cryptocurrency/quetzalCoin.py
@@ -73,10 +73,31 @@ class Blockchain:
     def add_node(self, address):
         parsed_url = urlparse(address)
         self.nodes.add(parsed_url.netloc)
+
+    def replace_chain (self):
+        network = self.nodes
+        longest_chain = None
+        max_length = len(self.chain)
+        for node in network:
+            response = requests.get(f'http://{node}/get_chain')
+            if response.status_code == 200:
+                length = response.json()['length']
+                chain = response.json()['chain']
+                if length > max_length and self.is_chain_valid(chain):
+                    max_length = length
+                    longest_chain = chain
+        if longest_chain:
+            self.chain = longest_chain
+            return True
+        return False
+
 #---------------------------------------- Part 2 - Mining our Blockchain
 
 # Creating a Web App
 app = Flask(__name__)
+
+# Creating an address for the node on Port 5000
+node_address = str(uuid4()).replace('-', '')
 
 # Creating a Blockchain
 blockchain = Blockchain()
@@ -88,12 +109,14 @@ def mine_block():
     previous_proof = previous_block['proof']
     proof = blockchain.proof_of_work(previous_proof)
     previous_hash = blockchain.hash(previous_block)
+    blockchain.add_transaction(sender = node_address, receiver = 'Zen', amount = 1)
     block = blockchain.create_block(proof, previous_hash)
     response = {'message': 'Congratulations, you just mined a block!',
                'index': block['index'],
                'timestamp': block['timestamp'],
                'proof': block['proof'],
-               'previous_hash': block['previous_hash']}
+               'previous_hash': block['previous_hash'],
+               'transactions': block['transactions']}
     return jsonify(response), 200
 
 # Getting the full Blockchain
@@ -112,15 +135,44 @@ def valid_chain():
         response = {'message': 'the chain is invalid'}
     return jsonify(response), 200
 
-
+# Adding a new transaction to the Blockchain
+@app.route("/add_transaction", methods=['POST'])
+def add_transaction():
+    json = request.get_json()
+    transaction_keys = ['sender', 'receiver', 'amount']
+    if not all (key in json for key in transaction_keys):
+        return 'Houston, we have a problem. Some elements of the transaction are missing', 400
+    index = blockchain.add_transaction(json['sender'], json['receiver'], json['amount'])
+    response = {'message': f'This transaction will be added to Block {index}'}
+    return jsonify(response), 201
 
 #---------------------------------------- Part 3 - Decentralizing our Blockchain
 
+# Connecting new nodes
 
+@app.route('/connect_node', methods = ['POST'])
+def connect_node():
+    json = request.get_json()
+    nodes = json.get('nodes')
+    if nodes is None:
+        return "No node", 400
+    for node in nodes:
+        blockchain.add_node(node)
+    response = {'message': 'All the nodes are now connected. The quetzal Blockchain now contains the following nodes:',
+                'total_nodes': list(blockchain.nodes)}
+    return jsonify(response), 201
 
-
-
-
+# Replacing the chain by the longest chain if needed
+@app.route("/replace_chain", methods=['GET'])
+def replace_chain():
+    is_chain_replaced = blockchain.replace_chain()
+    if is_chain_replaced:
+        response = {'message': 'The nodes had different chains so the chain was replaced by the longest one',
+                    'new_chain': blockchain.chain}
+    else:
+        response = {'message': 'All good. The chain is the largest one',
+                    'actual_chain': blockchain.chain}
+    return jsonify(response), 200
 #---------------------------------------- Running the app
 app.run(host = '0.0.0.0', port = 5000)
 
